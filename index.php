680192dcc53ec0c2124ddf54baa0cc91852b864d
niawaromi <91767702+niawaromi@users.noreply.github.com>
2022-03-19 09:36:28 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    //Loop
+    //Do while Loop
+    //For Loop
+    //Foreach Loop
+    $array = array("Daniel", "Jane", "Jacob", "John", "Mariane");
+
+     foreach ($array as $loopdata) { 
+         echo "My name is " .$loopdata. "<br>";
+     } 
+
+?>
+
+<body>
+    <html>
+        
+    </html>
+</body>
